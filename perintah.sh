cc46a54cf9671eb0eee4ad36cec4c8cf524cab4b
novalsaputra <novalsaputra0707@gmail.com>
2020-05-30 15:51:14 +0700

    edit

--- perintah.sh
+++ perintah.sh
@@ -6,6 +6,8 @@ mv gen.txt outputskripsi/tricluster1/gen.txt
 mv waktu.txt ouputskripsi/tricluster1/waktu.txt
 mv kondisi.txt outputskripsi/tricluster1/kondisi.txt
 
+cd ..
+
 git add .
 git commit -m 'tricluster1'
 git push origin master
