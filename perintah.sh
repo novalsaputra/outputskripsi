13b2846ce0ae12c34262d5eba472b4de5cee4b71
novalsaputra <novalsaputra0707@gmail.com>
2020-05-30 15:36:42 +0700

    edit perintah

---
+++ perintah.sh
@@ -0,0 +1,11 @@
+cd outputskripsi
+mkdir tricluster1
+
+mv gen.txt outputskripsi/tricluster1/gen.txt
+mv waktu.txt ouputskripsi/tricluster1/waktu.txt
+mv kondisi.txt outputskripsi/tricluster/waktu
+
+git add .
+git commit -m 'tricluster1'
+git push origin master
+
